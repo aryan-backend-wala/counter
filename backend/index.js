19d5998f84dd46694a2e832c89290b44981cd88c
Aryan Verma <aryan.computerscience@gmail.com>
2024-10-18 19:30:10 +0530

    final changes

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,12 @@ const secretKey = 'your-secret-key';
 app.use(express.json())
 const __dirname = path.resolve();
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")))
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  })
-}
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+} 
 
 let counter = 0;
 
